80390932f25cff243b849f068461c70cfef6113b
Marco Berri <marcoberri@gmail.com>
2019-11-29 10:00:44 +0100

    First Microservice with connection to config Server

--- configserver/README.md
+++ configserver/README.md
@@ -1,2 +1,4 @@
 Url to call for example
 http://localhost:8888/ms/prod/
+
+java -jar ms.jar --server.port=8083
---
+++ microservice/src/main/java/it/marcoberri/microservice/RestController.java
@@ -0,0 +1,25 @@
+package it.marcoberri.microservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+	@Value("${application.name}")
+	private String applicationName;
+
+	@Value("${application.author}")
+	private String applicationauthor;
+
+	@GetMapping(value = "/config/application/name", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String getApplicationName() {
+		return applicationName;
+	}
+
+	@GetMapping(value = "/config/application/author", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String getApplicationAuthor() {
+		return applicationauthor;
+	}
+}
